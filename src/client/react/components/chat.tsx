27631380448778198853448585b0399a0ac4b0e2
Damien Vesper <ldamienvesper@gmail.com>
2021-04-24 10:48:15 -0500

    chat render

--- src/client/react/components/chat.tsx
+++ src/client/react/components/chat.tsx
@@ -1,6 +1,6 @@
-import { Component } from 'react';
+import * as React from 'react';
 
-class Chat extends Component<{}, { messages: any[] }> {
+class Chat extends React.Component<{}, { messages: any[] }> {
     constructor (props) {
         super(props);
 
@@ -15,6 +15,24 @@ class Chat extends Component<{}, { messages: any[] }> {
 
     removeMsg = (id: number) => {
         this.setState({ messages: this.state.messages.filter(message => message.id !== id) });
+    }
+
+    render = () => (Element) => {
+        return (
+            <div className="chat">
+                {
+                    this.state.messages.map((message, i) => 
+                        <div className={`chat-msg ${message.fadeOut ? `chat-msg-fadeout`: ``}`}
+                            key={i}
+                            style={{ color: message.color === `red`
+                                            ? `pink`
+                                            : message.color === `blue`
+                                            ?  `cyan`
+                                            : `white` }}
+                        >{message.msg}</div>)
+                }
+            </div>
+        );
     }
 }
 
